0095b83c12511b25be72a4a6e8db023b07a8f552
jochemarends <contact@jochemarends.com>
2024-02-22 22:54:37 +0100

    same as previous

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod lexer;
+use lexer::Token;
 
 fn main() {
     if let Ok(tokens) = lexer::tokenize("1+2") {
@@ -8,6 +9,14 @@ fn main() {
     }
 }
 
-fn evaluate(tokens: &Vec<lexer::Token>) -> f64 {
+fn evaluate(text: &str) -> f64 {
+    if let Ok(tokens) = lexer::tokenize(text) {
+        for token in tokens {
+            match token {
+                _ => ()
+            }
+        }
+    }
 
+    0.0
 }
